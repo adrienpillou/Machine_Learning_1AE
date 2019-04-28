1c96de17d884d07ad9d845c37b7ac4ef81a43786
Adrien Pillou <44286944+adrienpillou@users.noreply.github.com>
2019-04-28 17:14:57 +0200

    Add files via upload

--- mslr-python/mlsr.py
+++ mslr-python/mlsr.py
@@ -1,32 +1,31 @@
 import cv2
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
 import random
 
-MODEL_NAME='mlsr_model-1556441977.model'
-CATEGORIES=["Circle", "Square", "Triangle"]
+MODEL_NAME='mlsr_model_1024-nodes_50-epochs.model'
+CATEGORIES=["cercle", "carré", "triangle"]#0-Cercle/ 1-Carre/ 2-Triangle
 TESTSET_DIM=999
 IMG_SIZE=32
 
-
 def prepare(filepath, index):
         img_array=cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-        new_array=cv2.resize(img_array,(IMG_SIZE,IMG_SIZE))
-        new_array=255-new_array
-        plt.subplot(2,6,index+1)
-        plt.imshow(new_array,cmap='gray')
+        img_array=255.0-img_array
+        new_array=cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+        plt.subplot(3,4,index+1)
+        plt.imshow(new_array, cmap='gray')
         plt.axis('off')
-        return new_array.reshape(-1,IMG_SIZE,IMG_SIZE,1)
+        return new_array.reshape(-1,IMG_SIZE,IMG_SIZE)
 
 model=tf.keras.models.load_model('./models/'+MODEL_NAME)
 
-
 for i in range(12):
         prediction=model.predict([prepare('./testset/test_'+str(random.randint(0,TESTSET_DIM))+'.png',i)])
-        #print(prediction)
-        output=CATEGORIES[int(prediction[0][0])]
-        print ('Image n° '+str(i+1)+' is probably a '+output+'.')
+        prediction_index=np.argmax(prediction)
+        output=CATEGORIES[prediction_index]
+        plt.title(str(i+1)+' : '+output)
+        print ("L' image n° "+str(i+1)+" est probablement un "+output+'.')
 
 plt.show()
-quit()
 
--- mslr-python/model_maker.py
+++ mslr-python/model_maker.py
@@ -1,17 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
-#TensorBoardhttp://localhost:6006
 from tensorflow.keras.callbacks import TensorBoard
 import pickle
 import time
 
 NAME="mlsr_model-{}".format(int(time.time()))
-iterations=100#Nombre d' 'epochs'
-
-conv_layers=3
-nodes=64
-dense_layers=0
+iterations=50#Nombre d' 'epochs'
+nodes=512
 
 pickle_in=open("X.pickle","rb")
 X=pickle.load(pickle_in)
@@ -21,31 +17,21 @@ Y=pickle.load(pickle_in)
 
 X=X/255.0
 
-#Structure du réseau cnn : 
-model=Sequential()
-board=TensorBoard(log_dir='logs/{}'.format(NAME))
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(32, 32)),
+  tf.keras.layers.Dense(nodes, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(3, activation='softmax')
+])
 
-#Couche noeuds :
-for i in range(conv_layers):
-        print('convolutional layer added !')
-        model.add(Conv2D(nodes, (3, 3), input_shape=X.shape[1:]))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Flatten())
-#Couche densification :
-for n in range(dense_layers):
-        print('densification layer added !')
-        model.add(Dense(nodes))
-        model.add(Activation('relu'))
-
-#Sortie :
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-
-model.compile(loss="binary_crossentropy",optimizer="adam",metrics=['accuracy'])
-model.fit(X, Y, batch_size=32, epochs=iterations, validation_split=.3, callbacks=[board])
+board=TensorBoard(log_dir='logs/{}'.format(NAME))#http://localhost:6006
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(X, Y, epochs=iterations,callbacks=[board])
 model.save('./models/'+NAME+'.model')
+
+
+
+
 
 
 
