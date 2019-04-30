112aaeeb1992058d8da70e5bbe1930cd70269fb7
Adrien Pillou <44286944+adrienpillou@users.noreply.github.com>
2019-04-30 11:27:50 +0200

    Ajout  de commentaires et réglages model.

--- MNIST/mnist.py
+++ MNIST/mnist.py
@@ -1,28 +1,37 @@
-import tensorflow as tf
-import matplotlib.pyplot as plt
+''' 
+Ce programme permet de créer et sauvegarder un model tf-keras
+dédié à MNIST.
 
-mnist = tf.keras.datasets.mnist
-nodes=32
+32 nodes @ 1 epochs : 85% acc
+128 nodes @ 3 epochs : 95% acc
+'''
 
-#Chargement de la dataset
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+import tensorflow as tf #Librairie TensorFlow
+import matplotlib.pyplot as pyplot  #Librairie affichage graphique
 
-#Structure du model cnn
+mnist = tf.keras.datasets.mnist #Dataset MNIST
+nodes=128 #Nombre de noeuds constituant la couche dense
+epochs_number=3
+
+#Chargement de la dataset : 
+(x_train, y_train),(x_test, y_test) = mnist.load_data() #Parsing de la dataset et répartition features/labels
+x_train, x_test = x_train / 255.0, x_test / 255.0 # Conversion gris(0-255)->gris(0-1)
+
+#Structure du model cnn : 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(nodes, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation='softmax')
+  tf.keras.layers.Flatten(input_shape=(28, 28)), #Conversion Matrice(28x28)/Vecteur(1x784)
+  tf.keras.layers.Dense(nodes, activation='relu'), #Couche de traitement dense
+  tf.keras.layers.Dropout(0.2), #???
+  tf.keras.layers.Dense(10, activation='softmax') #Couche de sortie constitué de 10 neuronnes
 ])
-#Compilation, entrainement et évaluation
+#Compilation, entrainement et évaluation : 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+              metrics=['accuracy']) #Compilation du model et réglages pour l'utilisation
 
-model.fit(x_train, y_train, epochs=1)
-model.evaluate(x_test, y_test)
-model.save('./models/mnist_model.model')
+model.fit(x_train, y_train, epochs=epochs_number) #Entrainement du model
+model.evaluate(x_test, y_test) #Evaluation du model sur des images inconnues
+model.save('./models/mnist_model.model') #Sauvegarde du model dans le répertoire 'models'
 
 
 
--- MNIST/predictions.py
+++ MNIST/predictions.py
@@ -1,26 +1,34 @@
+''' 
+Ce programme effectue 9 prédiction MNIST à partir 
+du model tf-keras dans situé dans le répertoire 'models'.
+'''
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import cv2
-import numpy as np
-import random
+import cv2 #Gestion des images 
+import numpy as np #Gestion des arrays
+import random #Géneration de nombre aléatoires
 
+#Chargement de la dataset MNIST :
 mnist=tf.keras.datasets.mnist
 (x_train,y_train),(x_test,y_test)=mnist.load_data()
 
+#Chargement du model :
 model=tf.keras.models.load_model('./models/mnist_model.model')
 
+#Affichage de quelques prédictions : 
 for i in range(9):
     random_index=random.randint(0,len(x_test))
-    random_image=x_test[random_index]
-    img_array=random_image.reshape(-1,28,28)
-    prediction=model.predict(img_array)
-    output=np.argmax(prediction[0])
-    answer=y_test[random_index]
-    plt.subplot(3,3,i+1)
-    plt.axis("off")
-    if output==answer:
-        plt.title('Correct',color='g')
+    random_image=x_test[random_index] #Choix d'une image parmis le testset
+    img_array=random_image.reshape(-1,28,28) #mise en forme de l'image
+    prediction=model.predict(img_array) #Prédiction à partir du model chargé
+    output=np.argmax(prediction[0]) #Mise en forme de la prédiction
+    answer=y_test[random_index] #Récupération de la réponse dans l'array des labels
+    plt.subplot(3,3,i+1) #Split des affichages
+    plt.axis("off") #Désactivation des axes
+    if output==answer: #Vérification de la prédiction
+        plt.title('Correct',color='g') #Prédiction correcte -> texte vert
     else:
-        plt.title('Error : '+str(output),color='r')
-    plt.imshow(random_image,cmap='gray_r')
-plt.show()
+        plt.title('Error : '+str(output),color='r') #Prédiction incorrecte -> texte rouge et affichage de la réponse
+    plt.imshow(random_image,cmap='gray_r') #Affichage de l'image sélectionnée
+plt.show()#Affichage Matplot
